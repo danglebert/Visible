393210f2ce6fa2a7413a3ad1e758fa8b6da76b97
Dan Gilbert <daniel.gilbert0051@gmail.com>
2019-05-30 11:06:37 -0400

    task: added animations upon opening app

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,7 @@ import {
   ImageBackground,
   KeyboardAvoidingView,
   Image,
+  Animated,
   TextInput
 } from 'react-native';
 import MainText from '../components/ui/MainText';
@@ -12,17 +13,38 @@ import backgroundImg from '../assets/purple-back.jpg';
 import teleIcon from '../assets/tv.png';
 
 export default class Home extends Component {
+  state = {
+    logoAnim: new Animated.Value(0),
+    inputAnim: new Animated.Value(0)
+  };
+
+  componentDidMount() {
+    Animated.timing(this.state.logoAnim, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true
+    }).start();
+
+    Animated.timing(this.state.inputAnim, {
+      toValue: 1,
+      duration: 2000,
+      delay: 2000,
+      useNativeDriver: true
+    }).start();
+  }
+
   render() {
+    const { logoAnim, inputAnim } = this.state;
     return (
       <ImageBackground source={backgroundImg} style={styles.backgroundImage}>
         <KeyboardAvoidingView style={styles.container}>
-          <View style={styles.logoWelcome}>
+          <Animated.View style={[styles.logoWelcome, { opacity: logoAnim }]}>
             <Image style={styles.tvIcon} source={teleIcon} />
             <MainText style={styles.mainText}>Welcome to Visible</MainText>
-          </View>
-          <View>
+          </Animated.View>
+          <Animated.View style={{ opacity: inputAnim }}>
             <MainText>Placeholder</MainText>
-          </View>
+          </Animated.View>
         </KeyboardAvoidingView>
       </ImageBackground>
     );
